b508298e3314a646ed453f75e33abc6675c57eec
klwwkl <2432875209@qq.com>
2023-06-01 09:41:28 +0800

    更新

--- js/index.js
+++ js/index.js
@@ -60,19 +60,19 @@ function timer() {
     }, 3000)
 }
 
-left.addEventListener("click", () => {
-    desc();
-    position();
-    clearInterval(time);
-    timer();
-})
+// left.addEventListener("click", () => {
+//     desc();
+//     position();
+//     clearInterval(time);
+//     timer();
+// })
 
-right.addEventListener("click", () => {
-    add();
-    position();
-    clearInterval(time);
-    timer();
-})
+// right.addEventListener("click", () => {
+//     add();
+//     position();
+//     clearInterval(time);
+//     timer();
+// })
 
 for (let i = 0; i < min.length; i++) {
     min[i].addEventListener("click", () => {
@@ -82,5 +82,117 @@ for (let i = 0; i < min.length; i++) {
         timer();
     })
 }
-
 timer();
+
+var navbox = document.querySelector(".nav-box");
+var f1 = document.querySelector(".f1");
+var rf = document.querySelector(".rf");
+var rn = document.querySelector(".rn");
+var reface = document.querySelector(".reface");
+var rename = document.querySelector(".rename");
+var xiala = document.querySelector(".xiala");
+var rfimg = reface.childNodes;
+var checkname = document.querySelector(".checkname");
+var username = document.querySelector("#username");
+var nowname = document.querySelector(".name");
+
+document.addEventListener('click', function (event) {
+    var clickedElement = event.target;
+    if (!clickedElement.closest('.wai')) {
+        xiala.style.display = 'none';
+        reface.style.display = 'none';
+        rename.style.display = 'none';
+    }
+});
+
+function recolor1(it) {
+    it.style.backgroundColor = "#f1f1f1";
+}
+function recolor2(it) {
+    it.style.backgroundColor = "#fff";
+}
+navbox.addEventListener("mouseover", () => {
+    recolor1(navbox);
+})
+navbox.addEventListener("mouseout", () => {
+    recolor2(navbox);
+})
+navbox.addEventListener("click", () => {
+    reface.style.display = 'none';
+    rename.style.display = 'none';
+    xiala.style.display = "block";
+})
+
+rf.addEventListener("click", () => {
+    xiala.style.display = "none";
+    reface.style.display = "flex";
+})
+rn.addEventListener("click", () => {
+    xiala.style.display = "none";
+    rename.style.display = "block";
+})
+
+rf.addEventListener("mouseover", () => {
+    recolor1(rf);
+})
+rf.addEventListener("mouseout", () => {
+    recolor2(rf);
+})
+
+rn.addEventListener("mouseover", () => {
+    recolor1(rn);
+})
+rn.addEventListener("mouseout", () => {
+    recolor2(rn);
+})
+
+rn.addEventListener("mouseover", () => {
+    recolor1(rn);
+})
+rn.addEventListener("mouseout", () => {
+    recolor2(rn);
+})
+
+
+for (let i = 0; i < rfimg.length; i++) {
+
+    rfimg[i].addEventListener("mouseover", () => {
+        recolor1(rfimg[i]);
+    })
+    rfimg[i].addEventListener("mouseout", () => {
+        recolor2(rfimg[i]);
+    })
+    let j = (i + 1) / 2;
+    rfimg[i].addEventListener("click", () => {
+        f1.style.backgroundImage = "url('../src/img/f" + j + ".jpg')";
+    })
+}
+
+navbox.addEventListener("click", () => {
+    xiala.style.display = "block";
+})
+
+var newname = "kk";
+checkname.addEventListener("click", () => {
+    newname = username.value;
+    nowname.textContent = newname;
+})
+
+function getElementToPageTop(el) {
+    if (el.parentElement) {
+        return this.getElementToPageTop(el.parentElement) + el.offsetTop
+    }
+    return el.offsetTop
+}
+window.addEventListener('scroll', function () {
+    var grade = document.querySelector('.grade');
+    if (window.pageYOffset > getElementToPageTop(grade)) {
+        grade.style.position = 'fixed';
+        grade.style.top = '0';
+        grade.style.right = '17%';
+    } else {
+        grade.style.position = 'absolute';
+        grade.style.top = 'none';
+        grade.style.right = '-43%';
+    }
+});
